8b99ce1cc995ead55ac418fe39b94dc1c321e139
Daniel <danielsaenz-96@gmail.com>
2020-03-21 18:33:11 -0500

    Connected to Contentful

--- src/components/Home/info.js
+++ src/components/Home/info.js
@@ -1,31 +1,29 @@
-import React from 'react'
-import {Link} from 'gatsby'
+import React from "react";
 import Title from "../Globals/Title";
-
-export default function info() {
-    return (
-        <div>
-            <section className="py-5">
-<div className="container">
-    <Title title="Our History"></Title>
-    <div className="row">
-        <div className="col-10 col-sm-8 mx-auto text-center">
-<p className="lead text-muted mb-5">
-Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. 
-Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
-Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
- Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a
- , venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. 
- Vivamus elementum semper nisi.</p>
-<Link to="/about/">
-    <button className="btn text-uppercase btn-purple">
-About page
-    </button>
-    </Link>
+import { Link } from "gatsby";
+export default function Info() {
+  return (
+    <section className="py-5">
+      <div className="container">
+        <Title title="our story" />
+        <div className="row">
+          <div className="col-10 col-sm-8 mx-auto text-center">
+            <p className="lead text-muted mb-5">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel amet
+              labore tempore delectus voluptate inventore? Iste tempore
+              molestias eaque rem quis blanditiis, facilis excepturi, magnam,
+              itaque earum sed fuga quia? At autem corporis ipsam libero
+              laboriosam! Unde autem nesciunt beatae, facere illum rerum
+              corporis nihil, sint, commodi in debitis quia.
+            </p>
+            <Link to="/about/">
+              <button className="btn text-uppercase btn-yellow">
+                about page
+              </button>
+            </Link>
+          </div>
         </div>
-    </div>
-</div>
-            </section>
-        </div>
-    )
+      </div>
+    </section>
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+
 import BackgroundSection from '../components/Globals/BackgroundSection'
 import Info from '../components/Home/info'
 
