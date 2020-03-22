b2b59319d30c9a0f4ad24f922eb4fb0b51147e7b
Daniel <danielsaenz-96@gmail.com>
2020-03-21 22:13:13 -0500

    Connected to Contentful

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,30 @@
-import React from "react"
-import { graphql } from "gatsby"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import React from "react";
+import { Link, graphql } from "gatsby";
 
-import BackgroundSection from '../components/Globals/BackgroundSection'
-import Info from '../components/Home/info'
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import BackgroundSection from "../components/Globals/BackgroundSection";
+import Info from "../components/Home/Info";
+import Menu from "../components/Home/Menu";
+import Products from "../components/Home/Products";
+import Contact from "../components/Home/Contact";
+const IndexPage = ({ data }) => {
+  return (
+    <Layout>
+      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+      <BackgroundSection
+        img={data.img.childImageSharp.fluid}
+        title="regular joe's"
+        styleClass="default-background"
+      />
+      <Info />
+      <Menu items={data.menu} />
+      <Products />
+      <Contact />
+    </Layout>
+  );
+};
 
-const IndexPage = ({data}) => (
-  <Layout>
-    <SEO title="Home"
-     keywords={[`gatsby`, `application`, `react`]} />
-    <BackgroundSection img={data.img.childImageSharp.fluid} 
-    tittle="uwu"
-     styleClass="default-background"/>
-
-     <Info />
-  </Layout>
-)
 export const query = graphql`
   {
     img: file(relativePath: { eq: "default-background.jpeg" }) {
@@ -25,10 +33,27 @@ export const query = graphql`
           ...GatsbyImageSharpFluid_tracedSVG
         }
       }
-
-}
     }
-`
 
+    menu: allContentfulCoffeeItem {
+      edges {
+        node {
+          id
+          title
+          description {
+            description
+          }
+          price
+          category
+          image {
+            fixed(width: 50, height: 50) {
+              ...GatsbyContentfulFixed_tracedSVG
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
-export default IndexPage
+export default IndexPage;
